7cfb22ffd664c5ebd7f447c0c917c2be45b651f1
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-22 18:00:23 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,15 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use((req, res) =>{
-    res.setHeader('Content-Type', 'application/json')
-    console.log('Posted:\n' + JSON.stringify(req.body))
-})
+// app.use((req, res) =>{
+//     res.setHeader('Content-Type', 'application/json')
+//     console.log('Posted:\n' + JSON.stringify(req.body))
+// })
 
 app.post('/logs', (req, res) => {
     let testPost = JSON.parse(req.body)
     console.log(testPost.test)
+    res.send(testPost.test)
 })
 
 app.listen(8080)
