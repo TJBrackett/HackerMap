039833db66a2972662f842060cd06dc90e6473d3
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-22 17:20:57 -0700

    Setting up backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.use((req, res) =>{
+    res.setHeader('Content-Type', 'application/json')
+    console.log('Posted:\n')
+    res.end(JSON.stringify(req.body))
+})
+
+app.post('/logs', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(8080)
