68c03ba4a4dbf01a8876634bb0585dbdc0611aa4
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-25 13:25:30 -0700

    Backend send/receives geolocation info

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
+//Imports private data such as API keys
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-//const access_key = require('info.env')
+const request = require('request')
 const app = express();
-const reqDate = new Date();
-const http = require('http')
 
+const reqDate = new Date();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+//Always called when the api is hit. Will use for logging.
 app.use((req, res, next) => {
     next()
 })
-
+//When address:port/logs is hit, execute.
 app.post('/logs', (req, res, next) => {
+//Save incoming info into a JSON object
     let postInfo = {
         ipAddr: req.body.ipAddr,
         reqType: req.body.reqType,
@@ -21,17 +24,19 @@ app.post('/logs', (req, res, next) => {
         reqStatus: req.body.reqStatus,
         reqUrl: req.body.reqUrl
     }
-    
-    const geoApi = "http://api.ipstack.com/" + postInfo.ipAddr + "?access_key=dc6b650b1595b16e1f0027954af5c1bd"
-
-    http.get(geoApi, (err, res) => {
-        let geoInfo = res
-
-        console.log(err)
-        console.log(geoInfo)
-    })
-    console.log('Posted:\n' + reqDate)
-    res.end("Post recieved successfully")
+//URL for ip geolocation lookup (Switching to Google Maps) 
+    const geoApi = "http://api.ipstack.com/" + postInfo.ipAddr + "?access_key=cb18c83bb4439722743b882ff7ac329d&format=1"
+//Calls the geolocation API
+    request(geoApi, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body.latitude)
+        console.log(body.longitude);
+        console.log(body.city);
+        console.log(body.region_name);
+        console.log(body.country_name);
+    });
+//Closes connection and sends final message.
+    res.end(postInfo.ipAddr)
 })
 
 app.listen(8080)
