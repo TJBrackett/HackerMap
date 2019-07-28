1b8e3d203b605d8d134f4d6196f47a1c6cbab792
Joseph M <joemark1989@gmail.com>
2019-07-27 22:42:52 -0400

    Moved all folders into hackermap and removed excess files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './dist/index.css';
+
+function App() {
+  return (
+    <div className="test">
+      <div className="circle"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/map/map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function map() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default map
