4439d80529448da59dc0af7b8c9b86aa8263ff7e
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-29 10:38:50 -0700

    Update logs.js

--- hackermap/backend/routes/logs.js
+++ hackermap/backend/routes/logs.js
@@ -22,14 +22,6 @@ app.post('/logs', (req, res, next) => {
     }
     //URL for ip geolocation lookup (Switching to Google Maps) 
     const geoApi = "http://api.ipstack.com/" + postInfo.ipAddr + "?access_key=" + process.env.GEO_IP + "&format=1"
-
-    //Add FK_IP 
-    // db.query('insert into site (siteURL, siteStatus, siteReqType, siteReqItem, siteCounter) values(?,?,?,?,?)',
-    //     [postInfo.reqUrl, postInfo.reqStatus, postInfo.reqType, postInfo.reqItem, 1], (err) => {
-    //         if (err) {
-    //             throw err
-    //         }
-    //     })
   
     //Calls the geolocation API
     request(geoApi, { json: true }, async (err, res, body) => {
@@ -44,11 +36,6 @@ app.post('/logs', (req, res, next) => {
                 country: body.country_name,
                 flag: body.location.country_flag_emoji_unicode
             }
-            //Also works, but await looks better and allows for return to be stored in a variable
-            // Promise.resolve(queryGeo(locationInfo.lat, locationInfo.long, locationInfo.city, locationInfo.region, locationInfo.country, locationInfo.flag))
-            // .then((pk_geo) => {
-            //     Promise.resolve(queryIp(pk_geo, postInfo.ipAddr))})
-            // .catch(err => console.log(err))
             
             const pk_geo = await queryGeo(locationInfo.lat, locationInfo.long, locationInfo.city, locationInfo.region, locationInfo.country, locationInfo.flag)
             const pk_ip = await queryIp(pk_geo, postInfo.ipAddr)
