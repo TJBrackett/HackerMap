be962b4b6d668577533488facfb6c242a12406c6
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-10-04 23:42:50 -0700

    SSE works & setup SMS

--- hackermap/backend/routes/logs.js
+++ hackermap/backend/routes/logs.js
@@ -6,13 +6,22 @@ const queryReq = require('../database/postReq.js')
 const querySite = require('../database/postSite.js')
 const queryVisits = require('../database/postVisits.js')
 const getAll = require('../database/getAll.js')
-const events = require('events')
-const em = new events.EventEmitter()
-em.setMaxListeners(0)
+const SSE = require('express-sse')
+const sse = new SSE(["Connected"], { isSerialized: false, initialEvent: 'Connected' })
+const sms = require('twilio')(process.env.TWILIO_ACCT, process.env.TWILIO_AUTH)
+const fs = require('fs')
+const router = express.Router()
+
+// const events = require('events')
+// const em = new events.EventEmitter()
+// em.setMaxListeners(0)
 
 //Allows for the logs module (everything in this file) to be called in app.js
 
 const app = module.exports = express()
+
+// Initializes server side events
+app.get('/', sse.init)
 
 //Called when a post request is made to http://localhost:PORT/logs
 
@@ -24,7 +33,7 @@ app.post('/logs', async (req, res, next) => {
     //Parse incoming json data
     //Req = request, body = the json object, ipAddr (etc) = the targeted field
 
-    const postInfo = {
+    let postInfo = {
         ipAddr: req.body.ipAddr,
         reqType: req.body.reqType,
         reqItem: req.body.reqItem,
@@ -59,42 +68,56 @@ app.post('/logs', async (req, res, next) => {
             const pk_visits = await queryVisits(pk_geo, pk_ip, pk_site, reqDate, reqTime)
             const pk_req = await queryReq(pk_site, pk_ip, postInfo.reqStatus, postInfo.reqItem, postInfo.reqType)
             //Setup the data to send to the front
-            const resJson = await {
+            const resJson = {
                 postInfo: postInfo,
                 locationInfo: locationInfo
             }
-            await em.emit("locationData", app.set("geoData", JSON.stringify(locationInfo))) //Stringify data being sent due to it having to be sent over text/event-stream
+            // em.emit("locationData", app.set("geoData", JSON.stringify(locationInfo))) //Stringify data being sent due to it having to be sent over text/event-stream
         }
     })
 
-    //Emits an event trigger whenever data is received to the post route
-    await em.emit("postData", app.set("sendData", JSON.stringify(postInfo))) //Stringify data being sent due to it having to be sent over text/event-stream
+    if (postInfo !== undefined && postInfo !== "") {
+        // Write keeps the connection open, where send or end would kill the connection to the front
+        await sse.send(`id: ${Date.now()}\ndata: ${JSON.stringify(postInfo)}\n\n`)
+
+        // Sends a text message when something happens
+        //     sms.messages
+        //         .create({
+        //          body: `${JSON.stringify(postInfo)}`,
+        //          from: `${process.env.TWILIO_PHONE}`,
+        //          to: `${process.env.TJ_PHONE}`
+        //     })
+        //        .then(message => console.log(message.sid))
+    }
+
     res.end()
 })
 
-//Called when a get request is made to http://localhost:PORT/logs
-app.get('/logs', async (req, res) => {
-    if (req.headers.accept == 'text/event-stream') {
-        //On a status 200(everythings ok), sets header to keep connection alive, don't cache, and the content type to text/eventstream
-        res.status(200).set({
-            "connection": "keep-alive", //Required
-            "cache-control": "no-cache", //Not required, but is a really good idea
-            "content-Type": "text/event-stream" //Required
-        })
+// Called when a get request is made to http://localhost:PORT/logs
 
-        //Once the postData event it triggered, send data
-        await em.on("postData", async () => {
-            let sendData = await app.get("sendData")
+// Going to be used to send initial data on page load
 
-            if (sendData !== undefined && sendData !== "") {
-                // Write keeps the connection open, where send or end would kill the connection to the front
-                await res.write(`id: ${Date.now()}\ndata: ${sendData}\n\n`)
-                // em.removeListener("postData", app.set("sendData", ""))
-                sendData = ""
-                em.removeAllListeners()
-            }
-        })
-    } else {
-        res.end("Your browser does not support event-streams.")
-    }
-})
+
+// app.get('/logs', async (req, res) => {
+    // if (req.headers.accept == 'text/event-stream') {
+    //     //On a status 200(everythings ok), sets header to keep connection alive, don't cache, and the content type to text/eventstream
+    //     res.status(200).set({
+    //         "connection": "keep-alive", //Required
+    //         "cache-control": "no-cache", //Not required, but is a really good idea
+    //         "content-Type": "text/event-stream" //Required
+    //     })
+    //Once the postData event it triggered, send data
+    // em.on("postData", async () => {
+    //     let sendData = await app.get("sendData")
+    //     if (sendData !== undefined && sendData !== "") {
+    //         // Write keeps the connection open, where send or end would kill the connection to the front
+    //         await res.write(`id: ${Date.now()}\ndata: ${sendData}\n\n`)
+    //         // em.removeListener("postData", app.set("sendData", ""))
+    //         sendData = ""
+    //         em.removeAllListeners()
+    //     }
+    // })
+    // } else {
+    //     res.end("Your browser does not support event-streams.")
+    // }
+// })
